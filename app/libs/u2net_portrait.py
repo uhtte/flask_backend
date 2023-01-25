920d4faf23a7e545077ab6edd55c56096a3304c6
Alex Kyoungdon Jang <uhtte.dev@gmail.com>
2023-01-25 17:36:04 +0900

    Update u2net_portrait.py

--- app/libs/u2net_portrait.py
+++ app/libs/u2net_portrait.py
@@ -183,8 +183,8 @@ class U2Net_portrait:
             # load u2net_portrait model
             net = U2NET(3, 1)
             net.load_state_dict(torch.load(model_dir, map_location=torch.device("cpu")))
-            if torch.cuda.is_available():
-                net.cuda()
+            # if torch.cuda.is_available():
+            #    net.cuda()
             net.eval()
 
             logger.info(f"run {filepath}")
